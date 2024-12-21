9e196440eed656c42e1c4a6570fc0805292e3a11
Hyunju-it <hyunjulee.it@gmail.com>
2024-12-21 20:33:00 +0900

    :sparkles: Feat: 원시형 자료타입 추가

---
+++ src/main/java/org/lab/data_type/PrimitiveType.java
@@ -0,0 +1,28 @@
+package main.java.org.lab.data_type;
+
+public class PrimitiveType {
+    /*
+        원시타입(primitive type)은 실제 값(call by value)을 직접 저장하고 호출하는 데이터 타입
+        - 각 타입은 크기와 범위가 정해져 있음
+        - 참조 타입(reference type)과는 실제 메모리에 할당되서 실행되는 구조가 다름
+     */
+    public static void main(String[] args) {
+
+        // 정수 자료형
+        byte number_byte = 127;
+        int number_int = 0;
+        short number_short = 0;
+        long number_long = 0L;
+
+        // 실수(부동소수점) 자료형
+        float number_float = 0.0F;
+        double number_double = 0.0D;
+
+        //문자
+        char number_char = '\u0000';
+
+        //논리
+        boolean number_boolean = false;
+
+    }
+}
